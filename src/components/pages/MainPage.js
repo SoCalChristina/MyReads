e181d4c7f2d5ffdf5d3ee9a961ea920b86ca4f32
Christina-Tovar-Hamernik <ctovarhamernik@gmail.com>
2018-11-12 17:14:43 -0800

    add API to MainPage

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import * as BooksAPI from '../../BooksAPI'
+import * as BooksAPI from '../../BooksAPI';
 
 import Shelf from '../Shelf';
 
 class MainPage extends React.Component {
+  componentDidMount() {
+      BooksAPI.getAll()
+      .then(resp => {
+        console.log(resp);
+      });
+    }
+
   render() {
     return (
-      <div>
       <div className="list-books">
         <div className="list-books-title">
           <h1>MyReads</h1>
@@ -22,7 +28,6 @@ class MainPage extends React.Component {
         <div className="open-search">
                   <Link to="/search">Add a book</Link>
                 </div>
-              </div>
               </div>
             );
           }
